9f03820eedff6497d2869e24e23740cf61d9630c
Maarten De Meyer <de.meyer.maarten@gmail.com>
2015-03-07 20:15:40 +0100

    Import from .functions

---
+++ checkinternet.sh
@@ -0,0 +1,27 @@
+# is the internet on fire status reports
+checkinternet() {
+    local title='Internet status:'
+    local cow='moose'
+    local term_length=40
+
+    if [ -n "$COLUMNS" ] && [ "$COLUMNS" -le 80 ] ; then
+        # Adapt the size of the message if the terminal supports it
+        term_length="$((COLUMNS-3))"
+    fi
+
+    local raw=$(dig +short txt istheinternetonfire.com)
+
+    # Chop quotes and add newline after sentence
+    # export INTERNETONFIRE for use in .bashrc
+    export INTERNETONFIRE=$(echo "$raw" | sed -e 's/"//'g -e 's/\.\s/\.\n\n/'g)
+
+    if [ $# -ne 0 ] && [ "$1" = '-fun' ] ; then
+        # paranoid moose for dramatic effect
+        printf "%s\n\n%s" "$title" "$INTERNETONFIRE" \
+            | cowsay -W "$term_length" -f "$cow" -p
+    else
+        # Boring/Professional mode
+        printf "%s\n%s" "$title" "$INTERNETONFIRE"
+    fi
+}
+
